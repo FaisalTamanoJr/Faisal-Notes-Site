6dfc61f7a34dc382e789680f77545771ba549cdf
Faisal Tamano Jr. <faisalrichardjr@gmail.com>
2024-06-12 18:27:37 +0800

    Quartz sync: Jun 12, 2024, 6:27 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,13 +37,13 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.MobileOnly(Component.Explorer()), Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
 }
